dccec3c028e8283909785cac7d913377cc671f07
Aliaksandr <51595644+ARomanowsky@users.noreply.github.com>
2020-11-01 19:30:52 +0300

    fix: update code

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,38 +1,22 @@
+const CustomError = require("../extensions/custom-error");
+
 module.exports = function transform(arr) {
-    if(typeof(arr) !== 'object') {
-        throw new Error();
+  if (!Array.isArray(arr)) throw Error
+  let subArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      subArr.push(arr[i+1]);
+      subArr.pop();
+      i+=2;
+    } else if (arr[i] === '--discard-prev' ) {
+      subArr.pop();
+    } else if (arr[i] === '--double-next') {
+      subArr.push(arr[i+1]);
+    } else if (arr[i] === '--double-prev') {
+      subArr.push(arr[i-1]);
+    } else {
+      subArr.push(arr[i]);
     }
-
-    if(!Array.isArray(arr)) {
-        throw new Error();
-    }
-
-    if(!arr.length) {
-        return arr;
-    }
-
-    const res = [];
-    for(let i = 0; i < arr.length; ++i) {
-        if(arr[i] === '--double-next') {
-            if(i < arr.length - 1) {
-                res.push(arr[i + 1]);
-            }
-        } else if(arr[i] === '--double-prev') {
-            if(i > 0) {
-                res.push(arr[i - 1]);
-            }
-        } else if(arr[i] === '--discard-next') {
-            if(i < arr.length - 1) {
-                i++;
-            }
-        } else if(arr[i] === '--discard-prev') {
-            if(res.length > 0) {
-                res.pop();
-            }
-        } else {
-            res.push(arr[i]);
-        }
-    }
-
-    return res;
+  }
+  return subArr.filter((i) => i !== undefined)
 };
