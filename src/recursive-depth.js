c2a00ddbb71bfe537b1defb55ba664cb9d4c0bb8
Aliaksandr <51595644+ARomanowsky@users.noreply.github.com>
2020-11-01 19:28:04 +0300

    fix: update code

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,25 +1,6 @@
-const CustomError = require("../extensions/custom-error");
-
 module.exports = class DepthCalculator {
-  
-  constructor() {
-    this.counter = 1;
-    this.level = 1;
-  }
-
-  calculateDepth(arr) {
-    for (let i = 0; i < arr.length; i += 1) {
-      if (Array.isArray(arr[i])) {
-        this.counter += 1;
-        this.calculateDepth(arr[i]);
-        if (this.counter > this.level) {
-          this.level = this.counter;
-          this.counter = 1;
-        } else {
-          this.counter = 1;
-        }
-      }
+    calculateDepth(arr) {
+        if(typeof(arr) !== "object") return 0;
+        return arr.reduce((max, it) => Math.max(this.calculateDepth(it), max), 0) + 1;
     }
-    return this.level;
-  }
 };
